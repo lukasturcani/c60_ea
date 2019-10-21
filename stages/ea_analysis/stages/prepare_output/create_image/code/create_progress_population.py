09238f05fd70ec3411551153d093761a30b53418
Lukas Turcani <lukasturcani93@gmail.com>
2019-10-21 13:36:23 +0200

    Added progress creation.

---
+++ stages/ea_analysis/stages/prepare_output/create_image/code/create_progress_population.py
@@ -0,0 +1,56 @@
+from utilities import get_database
+import argparse
+from os.path import join
+import pickle
+
+
+def _get_generations(log):
+    names = []
+    for line in log:
+        if line.isspace():
+            yield names
+            names = []
+        else:
+            names.append(line.split()[0])
+
+
+def get_progress(log_path, database_path):
+    cages = {cage.name: cage for cage in get_database(database_path)}
+
+    with open(log_path, 'r') as f:
+        log = f.readlines()
+
+    for names in _get_generations(log):
+        yield [cages[name] for name in names]
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'output_folder',
+        help=(
+            'The folder holding the output of the EA run. It will '
+            'hold the files "progress.log" and "database.json" '
+            'directly.'
+        ),
+    )
+    parser.add_argument(
+        'dump_path',
+        help=(
+            'The path to the file into which the pickled progress '
+            'is dumped.'
+        ),
+    )
+
+    args = parser.parse_args()
+    log_path = join(args.output_folder, 'progress.log')
+    database_path = join(args.output_folder, 'database.json')
+
+    progress = list(get_progress(log_path, database_path))
+
+    with open(args.dump_path, 'wb') as f:
+        pickle.dump(progress, f)
+
+
+if __name__ == '__main__':
+    main()
