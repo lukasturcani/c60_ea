3cf6a2a70b20d1d5e8d9ba2bf6a0104a5530b2da
Lukas Turcani <lukasturcani93@gmail.com>
2019-12-03 19:35:38 +0100

    added plot

--- stages/ea_analysis/stages/run_analysis/create_image/code/create_diversity_plot.py
+++ stages/ea_analysis/stages/run_analysis/create_image/code/create_diversity_plot.py
@@ -7,20 +7,37 @@ import matplotlib.pyplot as plt
 
 
 def get_fingerprint(molecule):
-    return rdkit.GetMorganFingerprint(molecule.to_rdkit_mol(), 2)
+    return rdkit.GetMorganFingerprint(molecule, 2)
 
 
 def similarity(molecule1, molecule2):
     return rdkit.DataStructs.DiceSimilarity(
-        bv1=get_fingerprint(molecule1),
-        bv2=get_fingerprint(molecule2),
+        get_fingerprint(molecule1),
+        get_fingerprint(molecule2),
     )
 
 
+def get_building_blocks(generation):
+    for cage in generation:
+        for building_block in cage.building_blocks:
+            rdkit.SanitizeMol(building_block.mol)
+            yield building_block.mol
+
+
+def dedupe(iterable):
+    seen = set()
+    for molecule in iterable:
+        key = rdkit.MolToInchi(molecule)
+        if key not in seen:
+            seen.add(key)
+            yield molecule
+
+
 def generation_similarity(generation):
+    building_blocks = list(dedupe(get_building_blocks(generation)))
     return np.mean([
         similarity(m1, m2)
-        for m1, m2 in it.product(generation, generation)
+        for m1, m2 in it.product(building_blocks, building_blocks)
     ])
 
 
